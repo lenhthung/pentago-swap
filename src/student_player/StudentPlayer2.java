4f9014b3d7e7bece680083ade7baf083f786e7d3
Le Nhat Hung <Le.nhthung@gmail.com>
2019-04-10 19:58:48 -0400

    Still timing out again

--- src/student_player/StudentPlayer2.java
+++ src/student_player/StudentPlayer2.java
@@ -48,7 +48,7 @@ public class StudentPlayer2 extends PentagoPlayer {
     		//boardState.printBoard();
     		
             //Move myMove = boardState.getRandomMove();
-    		myMove = ai.chooseMove(boardState, 1400);
+    		myMove = ai.chooseMove(boardState, 1300);
     	}
     	
     	// Return your move to be processed by the server.
